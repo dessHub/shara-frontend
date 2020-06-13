58f09e7da057d7342669ec93392e0e8a1b26de07
desshub <korirdesmond@MacBook-Pro-3.local>
2020-06-13 21:56:16 +0300

    Tools configurations

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import axios from 'axios'
 import Vue from 'vue'
+import VeeValidate from 'vee-validate'
+
 import App from './App'
 import router from './router'
 
+window.axios = axios
+axios.defaults.baseURL = 'http://127.0.0.1:3333'
+
 Vue.config.productionTip = false
+
+Vue.use(VeeValidate)
 
 /* eslint-disable no-new */
 new Vue({
