e57895e9de7a9eaaf5aa3d32fed6dc494803acc4
desshub <korirdesmond@MacBook-Pro-3.local>
2020-06-14 20:56:09 +0300

    Implement signup

--- src/main.js
+++ src/main.js
@@ -2,17 +2,26 @@
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import axios from 'axios'
 import Vue from 'vue'
-import VeeValidate from 'vee-validate'
+import en from 'vee-validate/dist/locale/en.json'
+import { ValidationObserver, ValidationProvider, extend, localize } from 'vee-validate'
+import * as rules from 'vee-validate/dist/rules'
 
 import App from './App'
 import router from './router'
 
 window.axios = axios
-axios.defaults.baseURL = 'http://127.0.0.1:3333'
+axios.defaults.baseURL = 'http://127.0.0.1:3333/api/v1'
+
+Object.keys(rules).forEach(rule => {
+  extend(rule, rules[rule])
+})
+
+localize('en', en)
 
 Vue.config.productionTip = false
 
-Vue.use(VeeValidate)
+Vue.component('ValidationObserver', ValidationObserver)
+Vue.component('ValidationProvider', ValidationProvider)
 
 /* eslint-disable no-new */
 new Vue({
