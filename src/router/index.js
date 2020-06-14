a0a75aeb6519fed0d56b87a7bed8c653fd320b96
desshub <korirdesmond@MacBook-Pro-3.local>
2020-06-14 21:47:44 +0300

    Login implementation

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import SignUpForm from '@/components/Auth/SignUpForm'
+import LogInForm from '@/components/Auth/LogInForm'
 
 Vue.use(Router)
 
@@ -15,6 +16,10 @@ export default new Router({
     {
       path: '/signup',
       component: SignUpForm
+    },
+    {
+      path: '/login',
+      component: LogInForm
     }
   ]
 })
